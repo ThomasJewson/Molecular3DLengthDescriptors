672186600e8c1833167b4dfa602cd5c280151d51
Thomas Jewson <tjewson1@gmail.com>
2021-05-06 23:36:19 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'Molecular3DLengthDescriptors',         # How you named your package folder (MyLib)
   packages = ['Molecular3DLengthDescriptors'],   # Chose the same as "name"
-  version = '1.0.1',      # Start with a small number and increase it with every change you make
+  version = '1.0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'A 3D conformational based molecular descriptor set.',   # Give a short description about your library
   author = 'Thomas Jewson',                   # Type in your name
