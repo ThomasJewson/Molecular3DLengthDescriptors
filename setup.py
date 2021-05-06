4b00fedde2c7d7bb87724c43bec6ba0900f40154
Thomas Jewson <tjewson1@gmail.com>
2021-05-06 18:10:21 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
   author = 'Thomas Jewson',                   # Type in your name
   author_email = 't.jewson1@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/ThomasJewson/Molecular3DLengthDescriptors',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/ThomasJewson/Molecular3DLengthDescriptors/archive/refs/tags/1.0.0.tar.gz',    # I explain this later on
   keywords = ['Molecular', 'Descriptor', '3D', 'Length', 'RDKit'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'validators',
